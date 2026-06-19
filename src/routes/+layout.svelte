78e3932b00a31b09b200117fda4dc5bc0cda8a15
gofreeil <yahavanter@gmail.com>
2026-06-19 03:36:54 +0300

    style: מרחק הבאנר מהראש 150px

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -61,7 +61,7 @@
 
     .hero-banner {
         width: 100%;
-        margin-top: 12px;
+        margin-top: 52px;
         margin-bottom: 1.5rem;
         border-radius: 1rem;
         overflow: hidden;
